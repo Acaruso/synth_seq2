7917ef478a455431795af5329a83f03c34007ffd
Acaruso <anthonycaruso102@gmail.com>
2021-10-19 08:36:02 -0400

    stuff

--- synth_seq2/src/main/ui_elements/advanced/transport_elt.cpp
+++ synth_seq2/src/main/ui_elements/advanced/transport_elt.cpp
@@ -1,8 +1,10 @@
 #include "transport_elt.hpp"
 
 #include "src/main/sequencer/sequencer.hpp"
+#include "src/main/ui_elements/advanced/number_with_label_elt.hpp"
 #include "src/main/ui_elements/advanced/rect_button_elt.hpp"
 #include "src/main/ui_elements/advanced/text_elt.hpp"
+#include "src/main/util.hpp"
 
 void playButtonElt(EltParams& params);
 void bpmElt(EltParams& params);
@@ -10,6 +12,7 @@ void bpmElt(EltParams& params);
 void transportElt(EltParams& params)
 {
     playButtonElt(params);
+    bpmElt(params);
 }
 
 void playButtonElt(EltParams& params)
@@ -47,7 +50,27 @@ void playButtonElt(EltParams& params)
 void bpmElt(EltParams& params)
 {
     auto& context = params.ctx;
+    auto& sequencer = context.sequencer;
     EltParams p(context);
     p.coord = params.coord;
-    p.coord.x += 50;
+    p.coord.x += 60;
+    p.coord.y += 20;
+    p.label = "bpm";
+
+    p.min = 0;
+    p.max = 999;
+
+    int maxNumDigits = (int)std::to_string(p.max).size();
+
+    int& data = sequencer->bpm;
+
+    p.displayText = pad(maxNumDigits, std::to_string(data));
+
+    p.onDrag = [&]() {
+        int drag = context.getDragAmount();
+        data = (drag == 1 || drag == -1) ? data + drag : data + (drag / 2);
+        data = clamp(data, p.min, p.max);
+    };
+
+    numberWithLabelElt(p);
 }
